f33ca0d69274494d36cbf26136e96f67fbba16de
KP_Hong <kphong16@daum.net>
2023-11-17 18:44:43 +0900

    Create RESTful backend todo app

---
+++ todo/serializers.py
@@ -0,0 +1,9 @@
+# todo/serializers.py
+
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'complete', 'important')
---
+++ todo/urls.py
@@ -0,0 +1,7 @@
+# todo/urls.py
+from django.urls import path
+from .views import TodosAPIView
+
+urlpatterns = [
+    path('todo/', TodosAPIView.as_view()),
+]
